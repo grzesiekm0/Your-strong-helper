3a55a01a97fe60e45226d10cc9e6d50b0e003c77
Grzegorz Macko <grzmacko@gmail.com>
2018-12-03 23:08:42 +0100

    New layout for log and reg have been adding

---
+++ app/src/main/java/com/yourstronghelper/grzegorzmacko/yourstronghelper/LogActivity.java
@@ -0,0 +1,113 @@
+package com.yourstronghelper.grzegorzmacko.yourstronghelper;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LogActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText mEmailView;
+    private EditText mPasswordView;
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        findViewById(R.id.textViewLog).setOnClickListener(this);
+        findViewById(R.id.buttonLog).setOnClickListener(this);
+
+        mEmailView = (EditText) findViewById(R.id.emailLog);
+        mPasswordView = (EditText) findViewById(R.id.passwordLog);
+        progressBar = (ProgressBar) findViewById(R.id.progressbar);
+
+
+    }
+
+    private void userLogin() {
+        String email = mEmailView.getText().toString().trim();
+        String password = mPasswordView.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            mEmailView.setError("Email is required");
+            mEmailView.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            mEmailView.setError("Please enter a valid email");
+            mEmailView.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            mPasswordView.setError("Password is required");
+            mPasswordView.requestFocus();
+            return;
+        }
+
+        if (password.length() < 6) {
+            mPasswordView.setError("Minimum lenght of password should be 6");
+            mPasswordView.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressBar.setVisibility(View.GONE);
+                if (task.isSuccessful()) {
+                    finish();
+                    Intent intent = new Intent(LogActivity.this, ProfileActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+/*
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (mAuth.getCurrentUser() != null) {
+            finish();
+            //startActivity(new Intent(this, ProfileActivity.class));
+        }
+    }*/
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.textViewLog:
+                finish();
+                startActivity(new Intent(this, RegActivity.class));
+                break;
+
+            case R.id.buttonLog:
+                userLogin();
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/yourstronghelper/grzegorzmacko/yourstronghelper/RegActivity.java
@@ -0,0 +1,109 @@
+package com.yourstronghelper.grzegorzmacko.yourstronghelper;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+
+public class RegActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText mEmailView;
+    private EditText mPasswordView;
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reg);
+
+        mEmailView = (EditText) findViewById(R.id.emailReg);
+        mPasswordView = (EditText) findViewById(R.id.passwordReg);
+        progressBar = (ProgressBar) findViewById(R.id.progressbar);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        findViewById(R.id.buttonReg).setOnClickListener(this);
+        findViewById(R.id.textViewReg).setOnClickListener(this);
+
+    }
+
+    private void registerUser() {
+        String email = mEmailView.getText().toString().trim();
+        String password = mPasswordView.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            mEmailView.setError("Email is required");
+            mEmailView.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            mEmailView.setError("Please enter a valid email");
+            mEmailView.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            mPasswordView.setError("Password is required");
+            mPasswordView.requestFocus();
+            return;
+        }
+
+        if (password.length() < 6) {
+            mPasswordView.setError("Minimum lenght of password should be 6");
+            mPasswordView.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressBar.setVisibility(View.GONE);
+                if (task.isSuccessful()) {
+                    finish();
+                    startActivity(new Intent(RegActivity.this, ProfileActivity.class));
+                } else {
+
+                    if (task.getException() instanceof FirebaseAuthUserCollisionException) {
+                        Toast.makeText(getApplicationContext(), "You are already registered", Toast.LENGTH_SHORT).show();
+
+                    } else {
+                        Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.buttonReg:
+                registerUser();
+                break;
+
+            case R.id.textViewReg:
+                finish();
+                startActivity(new Intent(this, RegActivity.class));
+                break;
+        }
+    }
+
+}
