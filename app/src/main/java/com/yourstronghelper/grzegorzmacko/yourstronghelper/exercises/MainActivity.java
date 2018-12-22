7ce0fddfe001c80c199ff44bb4f60f5930bc828f
Grzegorz Macko <grzmacko@gmail.com>
2018-12-22 14:02:01 +0100

    Removed old version activity with adding data for database. Old version has been added to the AddExerciseActivity. Added buttons and one click listener for  MainActivity.

--- app/src/main/java/com/yourstronghelper/grzegorzmacko/yourstronghelper/exercises/MainActivity.java
+++ app/src/main/java/com/yourstronghelper/grzegorzmacko/yourstronghelper/exercises/MainActivity.java
@@ -1,49 +1,44 @@
 package com.yourstronghelper.grzegorzmacko.yourstronghelper.exercises;
 
-import android.support.annotation.NonNull;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
 import com.yourstronghelper.grzegorzmacko.yourstronghelper.R;
 
-import java.util.HashMap;
-import java.util.Map;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity {
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
-
-    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Create a new user with a first and last name
-        Map<String, Object> user = new HashMap<>();
-        user.put("first", "Ada");
-        user.put("last", "Lovelace");
-        user.put("born", 1815);
 
-        // Add a new document with a generated ID
-        db.collection("users")
-                .add(user)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error adding document", e);
-                    }
-                });
+        findViewById(R.id.buttonAddExercise).setOnClickListener(this);
+        findViewById(R.id.buttonTrainingPlan).setOnClickListener(this);
+        findViewById(R.id.buttonGeneratePlan).setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.buttonAddExercise:
+
+                startActivity(new Intent(this, AddExerciseActivity.class));
+                break;
+
+            case R.id.buttonTrainingPlan:
+
+               // startActivity(new Intent(this, TO DO.class))
+                break;
+
+            case R.id.buttonGeneratePlan:
+
+                // startActivity(new Intent(this, TO DO.class))
+                break;
+
+
+        }
+    }
 }
