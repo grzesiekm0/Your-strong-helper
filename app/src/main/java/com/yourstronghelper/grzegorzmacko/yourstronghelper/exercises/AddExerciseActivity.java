38e7445b56465b1cc75eb6969782c6adfd484831
Grzegorz Macko <grzmacko@gmail.com>
2018-12-22 14:07:08 +0100

    Added activity for adding exercise to the gym.

---
+++ app/src/main/java/com/yourstronghelper/grzegorzmacko/yourstronghelper/exercises/AddExerciseActivity.java
@@ -0,0 +1,49 @@
+package com.yourstronghelper.grzegorzmacko.yourstronghelper.exercises;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.yourstronghelper.grzegorzmacko.yourstronghelper.R;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AddExerciseActivity extends AppCompatActivity {
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    private static final String TAG = "AddExerciseActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_exercise);
+        // Create a new user with a first and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
+
+        // Add a new document with a generated ID
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+    }
+
+}
