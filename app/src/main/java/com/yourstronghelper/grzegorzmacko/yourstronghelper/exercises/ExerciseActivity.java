ed3051bae73133e9ffc70b7a29fff11d6b8de1e4
Grzegorz Macko <grzmacko@gmail.com>
2018-12-23 14:27:08 +0100

    The functionality of displaying data from the server in the list has been created.

--- app/src/main/java/com/yourstronghelper/grzegorzmacko/yourstronghelper/exercises/ExerciseActivity.java
+++ app/src/main/java/com/yourstronghelper/grzegorzmacko/yourstronghelper/exercises/ExerciseActivity.java
@@ -1,13 +1,78 @@
 package com.yourstronghelper.grzegorzmacko.yourstronghelper.exercises;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.yourstronghelper.grzegorzmacko.yourstronghelper.R;
+import com.yourstronghelper.grzegorzmacko.yourstronghelper.model.Exercise;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ExerciseActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private ExerciseAdapter adapter;
+    private List<Exercise> exerciseList;
+    private ProgressBar progressBar;
+
+
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise);
+
+        progressBar = findViewById(R.id.progressbar);
+
+        recyclerView = findViewById(R.id.recyclerview_products);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        exerciseList = new ArrayList<>();
+        adapter = new ExerciseAdapter(this, exerciseList);
+
+        recyclerView.setAdapter(adapter);
+
+
+        db = FirebaseFirestore.getInstance();
+
+
+        db.collection("exercise").get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+
+                        progressBar.setVisibility(View.GONE);
+
+                        if (!queryDocumentSnapshots.isEmpty()) {
+
+                            List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
+
+                            for (DocumentSnapshot d : list) {
+
+                                Exercise p = d.toObject(Exercise.class);
+                                p.setId(d.getId());
+                                exerciseList.add(p);
+
+                            }
+
+                            adapter.notifyDataSetChanged();
+
+                        }
+
+
+                    }
+                });
+
     }
 }
--- app/src/main/java/com/yourstronghelper/grzegorzmacko/yourstronghelper/exercises/ExerciseAdapter.java
+++ app/src/main/java/com/yourstronghelper/grzegorzmacko/yourstronghelper/exercises/ExerciseAdapter.java
@@ -1,4 +1,76 @@
 package com.yourstronghelper.grzegorzmacko.yourstronghelper.exercises;
 
-public class ExerciseAdapter {
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.yourstronghelper.grzegorzmacko.yourstronghelper.R;
+import com.yourstronghelper.grzegorzmacko.yourstronghelper.model.Exercise;
+
+import java.util.List;
+
+public class ExerciseAdapter extends RecyclerView.Adapter<ExerciseAdapter.ExerciseViewHolder> {
+
+    private Context mCtx;
+    private List<Exercise> exerciseList;
+
+    public ExerciseAdapter(Context mCtx, List<Exercise> exerciseList) {
+        this.mCtx = mCtx;
+        this.exerciseList = exerciseList;
+    }
+
+    @NonNull
+    @Override
+    public ExerciseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ExerciseViewHolder(
+                LayoutInflater.from(mCtx).inflate(R.layout.layout_exercise, parent, false)
+        );
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ExerciseViewHolder holder, int position) {
+        Exercise exer = exerciseList.get(position);
+
+        holder.textViewName.setText(exer.getName());
+        holder.textViewBrand.setText(exer.getType());
+        holder.textViewDesc.setText(String.valueOf(exer.getSeries()));
+        holder.textViewPrice.setText(String.valueOf(exer.getQuantity()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return exerciseList.size();
+    }
+
+    class ExerciseViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        TextView textViewName, textViewBrand, textViewDesc, textViewPrice, textViewQty;
+
+        public ExerciseViewHolder(View itemView) {
+            super(itemView);
+
+            textViewName = itemView.findViewById(R.id.textview_name);
+            textViewBrand = itemView.findViewById(R.id.textview_brand);
+            textViewDesc = itemView.findViewById(R.id.textview_desc);
+            textViewPrice = itemView.findViewById(R.id.textview_price);
+            textViewQty = itemView.findViewById(R.id.textview_quantity);
+
+            itemView.setOnClickListener(this);
+
+        }
+
+        @Override
+        public void onClick(View v) {
+            /*Product product = productList.get(getAdapterPosition());
+            Intent intent = new Intent(mCtx, UpdateProductActivity.class);
+            intent.putExtra("product", product);
+            mCtx.startActivity(intent);*/
+        }
+    }
 }
+
