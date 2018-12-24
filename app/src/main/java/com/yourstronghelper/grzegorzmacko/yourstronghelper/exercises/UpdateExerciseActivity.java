4631b4a30398ea81da49dfbcb04dd4fb1059b9fe
Grzegorz Macko <grzmacko@gmail.com>
2018-12-24 15:02:25 +0100

    New layouts for updates exercises has been created.

---
+++ app/src/main/java/com/yourstronghelper/grzegorzmacko/yourstronghelper/exercises/UpdateExerciseActivity.java
@@ -0,0 +1,13 @@
+package com.yourstronghelper.grzegorzmacko.yourstronghelper.exercises;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UpdateExerciseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_exercise);
+    }
+}
