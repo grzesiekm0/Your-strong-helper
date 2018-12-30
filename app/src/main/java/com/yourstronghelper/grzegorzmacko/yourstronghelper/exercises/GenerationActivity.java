eb85351e8855fb41ba49a7e6ea5421e6619fb7cc
Grzegorz Macko <grzmacko@gmail.com>
2018-12-30 23:45:09 +0100

    .R has been imported.

--- app/src/main/java/com/yourstronghelper/grzegorzmacko/yourstronghelper/exercises/GenerationActivity.java
+++ app/src/main/java/com/yourstronghelper/grzegorzmacko/yourstronghelper/exercises/GenerationActivity.java
@@ -3,6 +3,8 @@ package com.yourstronghelper.grzegorzmacko.yourstronghelper.exercises;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.yourstronghelper.grzegorzmacko.yourstronghelper.R;
+
 public class GenerationActivity extends AppCompatActivity {
 
     @Override
