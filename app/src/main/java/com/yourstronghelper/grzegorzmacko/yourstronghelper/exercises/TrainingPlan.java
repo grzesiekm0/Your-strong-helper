3cddf6e2d32150b4136d8e8740ea6ab2e6876f9d
Grzegorz Macko <grzmacko@gmail.com>
2019-01-05 21:14:27 +0100

    New TrainingPlan activity has been added.

---
+++ app/src/main/java/com/yourstronghelper/grzegorzmacko/yourstronghelper/exercises/TrainingPlan.java
@@ -0,0 +1,13 @@
+package com.yourstronghelper.grzegorzmacko.yourstronghelper.exercises;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TrainingPlan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_training_plan);
+    }
+}
