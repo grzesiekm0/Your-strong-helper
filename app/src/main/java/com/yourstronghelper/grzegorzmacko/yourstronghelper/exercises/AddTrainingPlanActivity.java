69f54cf9d98fe442ca56c210a7d5d1a414835dc6
Grzegorz Macko <grzmacko@gmail.com>
2019-01-14 23:39:29 +0100

    Layout for added trainings and activity has been added

---
+++ app/src/main/java/com/yourstronghelper/grzegorzmacko/yourstronghelper/exercises/AddTrainingPlanActivity.java
@@ -0,0 +1,13 @@
+package com.yourstronghelper.grzegorzmacko.yourstronghelper.exercises;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddTrainingPlanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_training_plan);
+    }
+}
