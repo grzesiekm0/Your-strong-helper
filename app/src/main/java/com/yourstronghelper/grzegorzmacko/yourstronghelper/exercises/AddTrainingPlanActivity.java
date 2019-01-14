ab8e33812e3790233ad6d39612ed417f984d0fcc
Grzegorz Macko <grzmacko@gmail.com>
2019-01-14 23:43:06 +0100

    Displays exercises has been added for training acitvity.

--- app/src/main/java/com/yourstronghelper/grzegorzmacko/yourstronghelper/exercises/AddTrainingPlanActivity.java
+++ app/src/main/java/com/yourstronghelper/grzegorzmacko/yourstronghelper/exercises/AddTrainingPlanActivity.java
@@ -1,13 +1,106 @@
 package com.yourstronghelper.grzegorzmacko.yourstronghelper.exercises;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.yourstronghelper.grzegorzmacko.yourstronghelper.R;
+import com.yourstronghelper.grzegorzmacko.yourstronghelper.model.Exercise;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddTrainingPlanActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private ExerciseAdapter adapter;
+    private List<Exercise> exerciseList;
+    private ProgressBar progressBar;
+
+
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_training_plan);
+
+        progressBar = findViewById(R.id.progressbar_training);
+
+        recyclerView = findViewById(R.id.recyclerview_training_plan);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        exerciseList = new ArrayList<>();
+        adapter = new ExerciseAdapter(this, exerciseList);
+
+        recyclerView.setAdapter(adapter);
+
+
+        db = FirebaseFirestore.getInstance();
+
+
+        db.collection("exercise")
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+                        FirebaseUser user = mAuth.getCurrentUser();
+
+                        progressBar.setVisibility(View.GONE);
+
+                        if (!queryDocumentSnapshots.isEmpty()) {
+
+                            List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
+
+                            for (DocumentSnapshot d : list) {
+
+                                Exercise p = d.toObject(Exercise.class);
+                                p.setId(d.getId());
+                                System.out.println("p id "+p.getId());
+                                System.out.println("p idd "+p.getIdd());
+                                System.out.println("d "+user.getUid());
+                                if(p.getIdd().equals(user.getUid())){
+                                    exerciseList.add(p);
+                                }
+
+
+                            }
+
+                            adapter.notifyDataSetChanged();
+
+                        }
+
+
+                    }
+                });
+/*
+        db.collection("cities").get().then(function(querySnapshot) {
+            querySnapshot.forEach(function(doc) {
+                // doc.data() is never undefined for query doc snapshots
+                console.log(doc.id, " => ", doc.data());
+            });
+        });*/
+
     }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+        startActivity(new Intent(this, MainActivity.class));
+        return;
+    }
+
+
 }
