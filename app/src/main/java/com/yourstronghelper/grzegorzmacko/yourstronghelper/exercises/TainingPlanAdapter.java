36084fdea8f385e9e21b4d6bdd19e6a8cdc8cf42
Grzegorz Macko <grzmacko@gmail.com>
2019-01-14 23:46:02 +0100

    Adapter for list view for displays exercises has been created.

--- app/src/main/java/com/yourstronghelper/grzegorzmacko/yourstronghelper/exercises/TainingPlanAdapter.java
+++ app/src/main/java/com/yourstronghelper/grzegorzmacko/yourstronghelper/exercises/TainingPlanAdapter.java
@@ -1,4 +1,74 @@
 package com.yourstronghelper.grzegorzmacko.yourstronghelper.exercises;
 
-public class TainingPlanAdapter {
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.yourstronghelper.grzegorzmacko.yourstronghelper.R;
+import com.yourstronghelper.grzegorzmacko.yourstronghelper.model.Exercise;
+
+import java.util.List;
+
+public class TainingPlanAdapter extends RecyclerView.Adapter<TainingPlanAdapter.TrainingViewHolder>  {
+    private Context mCtx;
+    private List<Exercise> exerciseList;
+
+    public TainingPlanAdapter(Context mCtx, List<Exercise> exerciseList) {
+        this.mCtx = mCtx;
+        this.exerciseList = exerciseList;
+    }
+
+    @NonNull
+    @Override
+    public TainingPlanAdapter.TrainingViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new TainingPlanAdapter.TrainingViewHolder(
+                LayoutInflater.from(mCtx).inflate(R.layout.layout_add_training_plan, parent, false)
+        );
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TainingPlanAdapter.TrainingViewHolder holder, int position) {
+        Exercise exer = exerciseList.get(position);
+
+        holder.textViewName.setText(exer.getName());
+        holder.textViewBrand.setText(exer.getType());
+        holder.textViewDesc.setText(String.valueOf(exer.getSeries()));
+        holder.textViewPrice.setText(String.valueOf(exer.getQuantity()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return exerciseList.size();
+    }
+
+    class TrainingViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        TextView textViewName, textViewBrand, textViewDesc, textViewPrice, textViewQty;
+
+        public TrainingViewHolder(View itemView) {
+            super(itemView);
+
+            textViewName = itemView.findViewById(R.id.textview_name);
+            textViewBrand = itemView.findViewById(R.id.textview_brand);
+            textViewDesc = itemView.findViewById(R.id.textview_desc);
+            textViewPrice = itemView.findViewById(R.id.textview_price);
+            textViewQty = itemView.findViewById(R.id.textview_quantity);
+
+            itemView.setOnClickListener(this);
+
+        }
+
+        @Override
+        public void onClick(View v) {
+            Exercise exer = exerciseList.get(getAdapterPosition());
+            Intent intent = new Intent(mCtx, UpdateExerciseActivity.class);
+            intent.putExtra("exercise", exer);
+            mCtx.startActivity(intent);
+        }
+    }
 }
