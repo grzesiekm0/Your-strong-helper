26ed8f94e67fad2bd4747c7056d6c34d8eafbada
Grzegorz Macko <grzmacko@gmail.com>
2019-01-15 14:22:32 +0100

    onClicklistener for button in recycle view has been added.

--- app/src/main/java/com/yourstronghelper/grzegorzmacko/yourstronghelper/exercises/TainingPlanAdapter.java
+++ app/src/main/java/com/yourstronghelper/grzegorzmacko/yourstronghelper/exercises/TainingPlanAdapter.java
@@ -7,9 +7,12 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.yourstronghelper.grzegorzmacko.yourstronghelper.R;
+import com.yourstronghelper.grzegorzmacko.yourstronghelper.RegActivity;
 import com.yourstronghelper.grzegorzmacko.yourstronghelper.model.Exercise;
 
 import java.util.List;
@@ -39,6 +42,8 @@ public class TainingPlanAdapter extends RecyclerView.Adapter<TainingPlanAdapter.
         holder.textViewBrand.setText(exer.getType());
         holder.textViewDesc.setText(String.valueOf(exer.getSeries()));
         holder.textViewPrice.setText(String.valueOf(exer.getQuantity()));
+
+
     }
 
     @Override
@@ -49,6 +54,7 @@ public class TainingPlanAdapter extends RecyclerView.Adapter<TainingPlanAdapter.
     class TrainingViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         TextView textViewName, textViewBrand, textViewDesc, textViewPrice, textViewQty;
+        Button btn;
 
         public TrainingViewHolder(View itemView) {
             super(itemView);
@@ -58,17 +64,34 @@ public class TainingPlanAdapter extends RecyclerView.Adapter<TainingPlanAdapter.
             textViewDesc = itemView.findViewById(R.id.textview_desc);
             textViewPrice = itemView.findViewById(R.id.textview_price);
             textViewQty = itemView.findViewById(R.id.textview_quantity);
-
+           // btn = itemView.findViewById(R.id.btn);
+            this.btn = (Button) itemView.findViewById(R.id.btn);
+            btn.setOnClickListener(this);  // change number 1
             itemView.setOnClickListener(this);
 
         }
 
         @Override
         public void onClick(View v) {
+            int position = getAdapterPosition();
+            if(v.getId() == R.id.btn) {
+
+
+            }
+            switch (v.getId()) {
+                case R.id.btn:
+                    System.out.println("dupa");
+                    break;
+                /*case R.id.:
+                    System.out.println("dupa");
+                    break;*/
+            }
+
+            /*System.out.println("widok "+ v.getId());
             Exercise exer = exerciseList.get(getAdapterPosition());
             Intent intent = new Intent(mCtx, UpdateExerciseActivity.class);
             intent.putExtra("exercise", exer);
-            mCtx.startActivity(intent);
+            mCtx.startActivity(intent);*/
         }
     }
 }
