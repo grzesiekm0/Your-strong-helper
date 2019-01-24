c8745c6f1b1c2334501874f8136fc872904bd739
Grzegorz Macko <grzmacko@gmail.com>
2019-01-24 18:06:31 +0100

    Adapter for recycle view training plan has been modified

--- app/src/main/java/com/yourstronghelper/grzegorzmacko/yourstronghelper/exercises/PlanAdapter.java
+++ app/src/main/java/com/yourstronghelper/grzegorzmacko/yourstronghelper/exercises/PlanAdapter.java
@@ -1,4 +1,69 @@
 package com.yourstronghelper.grzegorzmacko.yourstronghelper.exercises;
 
-public class PlanAdapter {
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.yourstronghelper.grzegorzmacko.yourstronghelper.R;
+import com.yourstronghelper.grzegorzmacko.yourstronghelper.model.Exercise;
+import com.yourstronghelper.grzegorzmacko.yourstronghelper.model.TrainingPlan;
+
+import java.util.List;
+
+public class PlanAdapter extends RecyclerView.Adapter<PlanAdapter.PlanViewHolder> {
+
+    private Context mCtx;
+    private List<TrainingPlan> trainingPlanListList;
+
+    public PlanAdapter(Context mCtx, List<TrainingPlan> trainingPlanListList) {
+        this.mCtx = mCtx;
+        this.trainingPlanListList = trainingPlanListList;
+    }
+
+    @NonNull
+    @Override
+    public PlanViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new PlanViewHolder(
+                LayoutInflater.from(mCtx).inflate(R.layout.layout_training_plan, parent, false)
+        );
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PlanViewHolder holder, int position) {
+        TrainingPlan tp = trainingPlanListList.get(position);
+
+        holder.textViewName.setText(tp.getName());
+        holder.textViewBrand.setText(tp.getPlan());
+    }
+
+    @Override
+    public int getItemCount() {
+        return trainingPlanListList.size();
+    }
+
+    class PlanViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        TextView textViewName, textViewBrand, textViewDesc, textViewPrice, textViewQty;
+
+        public PlanViewHolder(View itemView) {
+            super(itemView);
+
+            textViewName = itemView.findViewById(R.id.textview_name);
+            textViewBrand = itemView.findViewById(R.id.textview_brand);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            /*Exercise exer = exerciseList.get(getAdapterPosition());
+            Intent intent = new Intent(mCtx, UpdateExerciseActivity.class);
+            intent.putExtra("exercise", exer);
+            mCtx.startActivity(intent);*/
+        }
+    }
 }
